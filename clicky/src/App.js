ba48cc6137957044852b8673ddde1c1e79f62b10
Mark Silva <mark.allen.silva@gmail.com>
2019-07-10 17:48:52 -0700

    working to get images to randomly generate

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -6,37 +6,38 @@ import NavBar from "./components/NavBar";
 import friends from "./friskyDingo.json";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
     friends
   };
 
-  // removeFriend = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-  //   const friends = this.state.friends.filter(friend => friend.id !== id);
-  //   // Set this.state.friends equal to the new friends array
-  //   this.setState({ friends });
-  // };
+//  function shuffle(friends) {
+//   const _friends = friends.slice(0)
+//   for (let i = 0; i < friends.length - 1; i++) {
+//     let randomIndex = Math.floor(math.random() * (i + 1))
+//     let temp = _friends[i]
+//     _friends[i] = _friends[randomIndex]
+//     _friends[randomIndex] = temp
+//   }
+// }
 
-  
 
-  // Map over this.state.friends and render a FaceCard component for each friend object
-  render() {
-    return (
-      <Wrapper>
-        <NavBar>Click Game</NavBar>
-        <Title>Click Game</Title>
-        {this.state.friends.map(friend => (
-          <FaceCard
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
+// Map over this.state.friends and render a FaceCard component for each friend object
+render() {
+  return (
+    <Wrapper>
+      <NavBar>Click Game</NavBar>
+      <Title>Click Game</Title>
+      {this.state.friends.map(friend => (
+        <FaceCard
+          id={friend.id}
+          key={friend.id}
+          name={friend.name}
+          image={friend.image}
+        />
+      ))}
+    </Wrapper>
+  );
+}
 }
 
 export default App;
